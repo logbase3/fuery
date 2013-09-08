0ac35dd07a580e68378e9f0c3a4605847f6bf80a
Roberto Lapuente <topo@asustin.net>
2013-09-08 16:07:26 -0500

    Move header declaration to global scope and make it constant.

--- main.go
+++ main.go
@@ -20,6 +20,7 @@
 */
 
 package main
+
 import (
 	"code.google.com/p/go-gnureadline"
 	"fmt"
@@ -27,40 +28,42 @@ import (
 )
 
 const HISTORY_FILE string = "my.history" // TODO: Change the config files to user home
-
-func main() {
-	header := "    fuery (File Query) Copyright (C) 2013  log₃()\n" +
+const HEADER string = "    Fuery (File Query) Copyright (C) 2013  log₃()\n" +
 	"    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n" +
 	"    This is free software, and you are welcome to redistribute it\n" +
 	"    under certain conditions; type `show c' for details.\n" +
 	"\n    For more information visit https://github.com/logbase3/fuery\n"
 
-	fmt.Printf("%s\n", header)
+var Delimiter byte = '|'
+
+func main() {
+	fmt.Printf("%s\n", HEADER)
 
 	var err error
 
 	term := os.ExpandEnv("TERM")
 	gnureadline.ReadHistory(HISTORY_FILE)
-	gnureadline.StifleHistory(100)  // Maximum 10 history entries
-	gnureadline.ReadInitFile(".inputrc")  // Read in a keybinding initialization file
+	gnureadline.StifleHistory(1000)      // Maximum history entries
+	gnureadline.ReadInitFile(".inputrc") // Read in a keybinding initialization file
 	line := ""
 
-	for i:=1; err == nil && line != "quit"; i++ {
-			line, err = gnureadline.Readline(fmt.Sprintf("fuery> "), true)
-			switch line {
-				case "vi":
-						gnureadline.Rl_editing_mode_set(gnureadline.Vi)
-				case "emacs":
-						gnureadline.Rl_editing_mode_set(gnureadline.Emacs)
-				case "insert":
-						gnureadline.Rl_insert_mode_set(true)
-				case "overwrite":
-						gnureadline.Rl_insert_mode_set(false)
-				}
-				fmt.Printf("You typed: %s\n", line)
+	for i := 1; err == nil && line != "quit"; i++ {
+		line, err = gnureadline.Readline(fmt.Sprintf("fuery> "), true)
+
+		switch line {
+		case "vi":
+			gnureadline.Rl_editing_mode_set(gnureadline.Vi)
+		case "emacs":
+			gnureadline.Rl_editing_mode_set(gnureadline.Emacs)
+		case "insert":
+			gnureadline.Rl_insert_mode_set(true)
+		case "overwrite":
+			gnureadline.Rl_insert_mode_set(false)
+		case "header":
+			fmt.Printf("%s\n", HEADER)
+		}
+		//fmt.Printf("You typed: %s\n", line)
 	}
-	fmt.Printf("History length %d\n",  gnureadline.HistoryLength())
-	fmt.Printf("History max entries %d\n",  gnureadline.HistoryMaxEntries())
 	gnureadline.WriteHistory(HISTORY_FILE)
 	gnureadline.Rl_reset_terminal(term)
 }
